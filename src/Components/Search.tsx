3d8ba49225da70e36fe34f6301ba081edbb37e25
Pavol <hudak.pav@gmail.com>
2023-08-25 23:04:00 +0200

    startdate

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../CSS/search.css'
 
 const Search: React.FC = () => {
+    function getCurrentDate() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
+        const day = now.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+    function getEndDate() {
+        const now = new Date();
+        now.setDate(now.getDate() + 7)
+        const year = now.getFullYear();
+        const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
+        const day = now.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
+    const [startDate, setStartDate] = useState<string>(getCurrentDate());
+    const [endDate, setEndDate] = useState<string>(getEndDate());
+    useEffect(() => {
+        setStartDate(getCurrentDate());
+        setEndDate(getEndDate());
+      }, []);
+
     return(
         <div className='searchContainer'>
             <form>
-                <input className='date-input' type='date'></input>
-                <input className='date-input' type='date'></input>
+                <input className='date-input' type='text' placeholder="Check-in" value={startDate} onChange={(e) => setStartDate(e.target.value)}></input>
+                <input className='date-input' type='text' placeholder="Check-out" value={endDate} onChange={(e) => setEndDate(e.target.value)}></input>
                 <div className='search-persons'>
                     <input className='person-input' type='number'></input>
                     <input className='person-input' type='number'></input>
