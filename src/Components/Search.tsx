d6d8cbbb9f2a553054d62f6543b4e35925844580
Pavol <hudak.pav@gmail.com>
2023-08-26 15:21:36 +0200

    Date input fixed

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,35 +1,54 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import '../CSS/search.css'
 
 const Search: React.FC = () => {
-    function getCurrentDate() {
-        const now = new Date();
-        const year = now.getFullYear();
-        const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
-        const day = now.getDate().toString().padStart(2, '0');
-        return `${year}-${month}-${day}`;
-    }
-    function getEndDate() {
-        const now = new Date();
-        now.setDate(now.getDate() + 7)
-        const year = now.getFullYear();
-        const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
-        const day = now.getDate().toString().padStart(2, '0');
-        return `${year}-${month}-${day}`;
-    }
+    const [rawDate, setRawDate] = useState<string>('');
+  const [formattedCheckInDate, setFormattedCheckInDate] = useState<string>('');
+  const [formattedCheckOutDate, setFormattedCheckOutDate] = useState<string>('');
 
-    const [startDate, setStartDate] = useState<string>(getCurrentDate());
-    const [endDate, setEndDate] = useState<string>(getEndDate());
-    useEffect(() => {
-        setStartDate(getCurrentDate());
-        setEndDate(getEndDate());
-      }, []);
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const input = e.target.value;
+    
+    // Remove any non-numeric characters from the input
+    const numericInput = input.replace(/\D/g, '');
+
+    // Split the numeric input into day, month, and year
+    const day = numericInput.slice(0, 2);
+    const month = numericInput.slice(2, 4);
+    const year = numericInput.slice(4, 8);
+
+    // Format the date as "DD/MM/YYYY"
+    let formatted = '';
+
+  if (numericInput.length >= 1) {
+    formatted += day;
+  }
+
+  if (numericInput.length >= 3) {
+    formatted += `/${month}`;
+  }
+
+  if (numericInput.length >= 5) {
+    formatted += `/${year}`;
+  }
+  if(name === 'checkIn-input'){
+    setFormattedCheckInDate(formatted);
+  }
+  else if(name === 'checkOut-input'){
+    setFormattedCheckOutDate(formatted)
+  }
+
+    
+    // Set the raw date in state (numeric input)
+    setRawDate(numericInput);
+  };
 
     return(
         <div className='searchContainer'>
             <form>
-                <input className='date-input' type='text' placeholder="Check-in" value={startDate} onChange={(e) => setStartDate(e.target.value)}></input>
-                <input className='date-input' type='text' placeholder="Check-out" value={endDate} onChange={(e) => setEndDate(e.target.value)}></input>
+                <input name='checkIn-input' className='date-input' type='text' placeholder="Check-in" value={formattedCheckInDate} onChange={handleChange}></input>
+                <input name='checkOut-input' className='date-input' type='text' placeholder="Check-out" value={formattedCheckOutDate} onChange={handleChange}></input>
                 <div className='search-persons'>
                     <input className='person-input' type='number'></input>
                     <input className='person-input' type='number'></input>
